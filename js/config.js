db368d56c805663cbfdd332c136c42a3828c31ee
Andrey Reinz <110102592+AndreyReinz@users.noreply.github.com>
2023-12-24 11:51:29 +0700

    Update config.js
    Signed-off-by: Andrey Reinz <110102592+AndreyReinz@users.noreply.github.com>

--- js/config.js
+++ js/config.js
@@ -13,7 +13,7 @@ var l_serverImage = "images/logo.png";
 
 /*Custom server name configuration
 Leave it blank for automatic server name*/
-var l_serverName = "MTF | SCP Epsilon-6";
+var l_serverName = "The Last Of Us";
 
 /*Use a video for the background?
 Otherwise it will just use images*/
@@ -102,8 +102,8 @@ var l_messagesEnabled = true;
 
 /*Enter your custom messages below*/
 var l_messages = [
-"Добро пожаловать на 'MTF | SCP Epsilon-6'",
-"Cпасибо, что присоединились к 'SCP Epsilon-6'",
+"Добро пожаловать на 'The Last Of Us'",
+"Cпасибо, что присоединились к 'The Last Of Us'",
 "Удачной вам игры!"
 ];
 
